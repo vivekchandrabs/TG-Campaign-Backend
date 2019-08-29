da3eb0dc87cd797c67fdf2a3ddb4c6e97cec320b
Vivek Chandra B S <vivek.chandra@speckbit.com>
2019-08-29 19:12:05 +0530

    check for bugs

--- telegram_bot/tasks.py
+++ telegram_bot/tasks.py
@@ -7,31 +7,31 @@ from telegram_bot.models import Post, Series
 
 @app.task
 def send_message(series):
-    
-    series = Series.objects.get(pk=series)
-    chat_id = series.group_id
-    api_key = series.api_key
+	
+	series = Series.objects.get(pk=series)
+	chat_id = series.group_id
+	api_key = series.api_key
 
-    posts = Post.objects.filter(series=series, is_sent=False)
+	posts = Post.objects.filter(series=series, is_sent=False)
 
-    if posts.exists():
-    	post = posts.first()
-    	post.is_sent = True
-    	post.save()
+	if posts.exists():
+		post = posts.first()
+		post.is_sent = True
+		post.save()
 
-    	title = post.title
-    	content = post.content
+		title = post.title
+		content = post.content
 
-    	url = f"https://api.telegram.org/bot{api_key}/sendMessage?chat_id={chat_id}&text={content}&parse_mode=Markdown"
-    	data = requests.get(url)
+		url = f"https://api.telegram.org/bot{api_key}/sendMessage?chat_id={chat_id}&text={content}&parse_mode=Markdown"
+		data = requests.get(url)
 
-    if len(posts) == 1:
-        print("ksfdjksdfkhskfhskdfhksdfhksfhdkshdfkhfk")
-    	series.is_sent = True
-    	series.save()
+	if len(posts) == 1:
+		print("ksfdjksdfkhskfhskdfhksdfhksfhdkshdfkhfk")
+		series.is_sent = True
+		series.save()
 
-    	series.periodic_task.enabled = False
-    	series.periodic_task.save()
+		series.periodic_task.enabled = False
+		series.periodic_task.save()
 
 
 
