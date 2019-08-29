7d34894e8a70a0f2965cb1644e8d30dbfa6b17d9
Vivek Chandra B S <vivek.chandra@speckbit.com>
2019-08-29 13:19:36 +0530

    Add api key

--- telegram_bot/tasks.py
+++ telegram_bot/tasks.py
@@ -8,12 +8,11 @@ from telegram_bot.models import Post, Series
 @app.task
 def send_message(series):
     
-    print("Beat is sending and the worker is receiving")
     series = Series.objects.get(pk=series)
     chat_id = series.group_id
+    api_key = series.api_key
+    
     posts = Post.objects.filter(series=series, is_sent=False)
-    print(posts)
-    print(series.enabled)
     if posts.exists():
     	post = posts.first()
     	post.is_sent = True
@@ -22,7 +21,7 @@ def send_message(series):
     	title = post.title
     	content = post.content
 
-    	url = f"https://api.telegram.org/bot981855943:AAHElrIJ01s9MeL_3w1vBAmgCAkb2DpFl2A/sendMessage?chat_id={chat_id}&text={content}&parse_mode=Markdown"
+    	url = f"https://api.telegram.org/bot{api_key}/sendMessage?chat_id={chat_id}&text={content}&parse_mode=Markdown"
     	data = requests.get(url)
 
     else:
